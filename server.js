5061d8e1de3d9fec138bacf90e1416db9e1ccf71
Priyank <priyankmadan98@gmail.com>
2019-04-03 22:18:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.Server(app)
+const io = socketio(server)
+
+let idUserMap = {}
+
+io.on('connection', (socket) => {
+  console.log('Connected ' + socket.id)
+
+  socket.on('login', (data) => {
+    idUserMap[socket.id] = data.username
+    socket.emit('loggedin')
+  })
+
+  socket.on('chat', (data) => {
+    io.emit('chat_rcvd', {
+      username: idUserMap[socket.id],
+      msg: data.msg
+    })
+  })
+})
+
+
+app.use('/', express.static(
+  __dirname + '/public'
+))
+
+server.listen(3333, () => {
+  console.log(`Server started on
+  http://localhost:3333`)
+})
